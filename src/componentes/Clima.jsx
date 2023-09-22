5f9e1cd0c5b09cb108be194c6d3a03bf668cf48a
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-09-22 20:01:30 -0300

    Creado 2 componentes como base para ambas Dashboards con la pantalla dividida en el medio

---
+++ src/componentes/Clima.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Clim = styled.div`
+    width:50%;
+    min-height: 100vh;
+`
+
+const Clima = () => {
+  return (
+    <Clim>Clima</Clim>
+  )
+}
+
+export default Clima
---
+++ src/componentes/Transito.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Trans = styled.div`
+    width:50%;
+    min-height: 100vh;
+    border-left:1px solid white;
+`
+
+const Transito = () => {
+  return (
+    <Trans>Transito</Trans>
+  )
+}
+
+export default Transito
