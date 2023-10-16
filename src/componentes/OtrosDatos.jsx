21fc14057b7b39d354fb6e9e5195ecfa0b8fdf6d
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-10-16 13:35:16 -0300

    Se cambió el diseño de OtrosDatos.jsx de 'grid' a 'flex', simplificando tódo el código y evitando repeticiones no necesarias.

--- src/componentes/OtrosDatos.jsx
+++ src/componentes/OtrosDatos.jsx
@@ -1,160 +1,101 @@
 import React from 'react'
 import styled from 'styled-components'
 
+  // Otros tiene "display: flex" con "flex-dirección" default en "row"
 const Otros = styled.div`
     grid-area: 2 / 1 / 4 / 4;
     height:100%;
     width:100%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    gap: 2rem;
+    display: flex;
 `
-
-const A1 = styled.div`
-    grid-area: 1 / 1 / 1 / 1;
+  // Sección A y B actúan como columnas en pantallas anchas, pero permiten verse como filas en pantallas angostas.
+const SeccionA = styled.div`
+    height:100%;
+    width:50%;
+    display: flex;
+    flex-direction: column;
+    padding:0.5rem;
+`
+const SeccionB = styled.div`
+    height:100%;
+    width:50%;
+    display: flex;
+    flex-direction: column;
+    padding:0.5rem;
+`
+  // El formato de cada rectángulo que muestra un dato.
+const Dato = styled.div`
     height:100%;
     width:100%;
     background:linear-gradient(315deg, #adadad, white);
     border-radius: 10px;
     box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    padding:1rem;
+    margin:0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
-
-const B1 = styled.div`
-    grid-area: 1 / 2 / 1 / 2;
-    height:100%;
-    width:100%;
-    background:linear-gradient(315deg, #adadad, white);
-    border-radius: 10px;
-    box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    padding:1rem;
+  // Determina el tamaño de fuente del título de cada rectángulo.
+const Titulo = styled.p`
+    font-size: 1.5rem;
 `
-
-const A2 = styled.div`
-    grid-area: 2 / 1 / 2 / 1;
-    height:100%;
-    width:100%;
-    background:linear-gradient(315deg, #adadad, white);
-    border-radius: 10px;
-    box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    padding:1rem;
+  // Determina el tamaño de fuente del valor de cada rectángulo.
+const Valor = styled.p`
+    font-size: 1.5rem;
 `
-
-const B2 = styled.div`
-    grid-area: 2 / 2 / 2 / 2;
-    height:100%;
-    width:100%;
-    background:linear-gradient(315deg, #adadad, white);
-    border-radius: 10px;
-    box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    padding:1rem;
-`
-
-const A3 = styled.div`
-    grid-area: 3 / 1 / 3 / 1;
-    height:100%;
-    width:100%;
-    background:linear-gradient(315deg, #adadad, white);
-    border-radius: 10px;
-    box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    padding:1rem;
-`
-
-const A3A = styled.p`
-    grid-area: 1 / 1 / 1 / 1;
-`
-
-const A3B = styled.p`
-    grid-area: 1 / 2 / 1 / 2;
-`
-
-const A3C = styled.p`
-    grid-area: 2 / 1 / 2 / 1;
-`
-
-const A3D = styled.p`
-    grid-area: 2 / 2 / 2 / 2;
-`
-
-const B3 = styled.div`
-    grid-area: 3 / 2 / 3 / 2;
-    height:100%;
-    width:100%;
-    background:linear-gradient(315deg, #adadad, white);
-    border-radius: 10px;
-    box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-    padding:1rem;
-`
-
-const Humedad = ({humed}) => {
-  return (
-    <A1>
-      <p>Humedad</p>
-      <p>{humed}%</p>
-    </A1>
-  )
-}
-
-const UV = ({uvInd}) => {
-  return (
-    <B1>
-      <p>Indice UV</p>
-      <p>{uvInd}</p>
-    </B1>
-  )
-}
-
-const Viento = ({vientoV}) => {
-  return (
-    <A2>
-      <p>Velocidad del Viento</p>
-      <p>{vientoV} km/h</p>
-    </A2>
-  )
-}
-
-const Visibilidad = ({visib}) => {
-  return (
-    <B2>
-      <p>Visibilidad</p>
-      <p>{visib} km</p>
-    </B2>
-  )
-}
-
-const Amanecer = ({amanec,anoch}) => {
-  return (
-    <A3>
-      <A3A>Amanece:</A3A>
-      <A3B>Anochece:</A3B>
-      <A3C>{amanec} hs</A3C>
-      <A3D>{anoch} hs</A3D>
-    </A3>
-  )
-}
-
-const Calidad = ({calid}) => {
-  return (
-    <B3>
-      <p>Calidad del Aire</p>
-      <p>{calid}</p>
-    </B3>
-  )
-}
 
 const OtrosDatos = ({humed,vientoV,vientoD,uvInd,amanec,anoch,visib,calid}) => {
   return (
     <Otros>
-      <Humedad humed={humed}/>
-      <UV uvInd={uvInd}/>
-      <Viento vientoV={vientoV} vientoD={vientoD}/>
-      <Visibilidad visib={visib}/>
-      <Amanecer amanec={amanec} anoch={anoch}/>
-      <Calidad calid={calid}/>
+  
+      <SeccionA>
+  
+        <Dato>
+          <Titulo>Amanece:</Titulo>
+          <Valor>{amanec} hs</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Anochece:</Titulo>
+          <Valor>{anoch} hs</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Humedad:</Titulo>
+          <Valor>{humed}%</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Indice UV:</Titulo>
+          <Valor>{uvInd}</Valor>
+        </Dato>
+
+      </SeccionA>
+
+      <SeccionB>
+
+        <Dato>
+          <Titulo>Velocidad del Viento:</Titulo>
+          <Valor>{vientoV} km/h</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Dirección del Viento:</Titulo>
+          <Valor>{vientoD}°</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Visibilidad:</Titulo>
+          <Valor>{visib} km</Valor>
+        </Dato>
+
+        <Dato>
+          <Titulo>Calidad del Aire:</Titulo>
+          <Valor>{calid}</Valor>
+        </Dato>
+
+      </SeccionB>
+  
     </Otros>
   )
 }
