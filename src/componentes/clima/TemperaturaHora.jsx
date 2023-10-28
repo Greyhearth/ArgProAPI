6ae16c723bb6e234be38d2ef2a411eed5f88e88c
Gustavo <gustavo.grisetti@mi.unc.edu.ar>
2023-10-28 11:34:53 -0300

    ligeras modificaciones a TemperaturaHora.jsx

--- src/componentes/clima/TemperaturaHora.jsx
+++ src/componentes/clima/TemperaturaHora.jsx
@@ -19,25 +19,15 @@ const TempHora = styled.p`
 `
 
 const TemperaturaHora = ({tempHora,horaT}) => {
-  const dataTemp = tempHora
-  const dataHora = horaT
+    // Al componente le llega 2 arrays con los datos por hora de la temperatura y sus horarios.
+    // 'data' se construye obteniendo un valor del array 'tempHora', y asociandolo con el correspondiente del array 'horaT' en posición 'i'.
+    // Al elemento de 'horaT' en posición 'i' se le aplica '.splice()' para que sólo muestre el valor de la hora.
+   const data = tempHora.map ((temp,i)=> {
+    return (
+      {name: horaT[i].slice(11,13) + "hs", Temp: temp}
+    )});
 
-  const data = [
-    {name: dataHora[0].slice(11,13)+"hs", Temp: dataTemp[0], pv: 240, amt: 240},
-    {name: dataHora[2].slice(11,13)+"hs", Temp: dataTemp[2], pv: 240, amt: 240},
-    {name: dataHora[4].slice(11,13)+"hs", Temp: dataTemp[4], pv: 240, amt: 240},
-    {name: dataHora[6].slice(11,13)+"hs", Temp: dataTemp[6], pv: 240, amt: 240},
-    {name: dataHora[8].slice(11,13)+"hs", Temp: dataTemp[8], pv: 240, amt: 240},
-    {name: dataHora[10].slice(11,13)+"hs", Temp: dataTemp[10], pv: 240, amt: 240},
-    {name: dataHora[12].slice(11,13)+"hs", Temp: dataTemp[12], pv: 240, amt: 240},
-    {name: dataHora[14].slice(11,13)+"hs", Temp: dataTemp[14], pv: 240, amt: 240},
-    {name: dataHora[16].slice(11,13)+"hs", Temp: dataTemp[16], pv: 240, amt: 240},
-    {name: dataHora[18].slice(11,13)+"hs", Temp: dataTemp[18], pv: 240, amt: 240},
-    {name: dataHora[20].slice(11,13)+"hs", Temp: dataTemp[20], pv: 240, amt: 240},
-    {name: dataHora[22].slice(11,13)+"hs", Temp: dataTemp[22], pv: 240, amt: 240},
-  ];
-  
-  const renderLineChart = (
+  const graficoTemp = (
     <ResponsiveContainer width="95%" height="80%">
       <AreaChart width={850} height={160} margin={{ top: 5, left: 0, right: 0, bottom: 0 }}  data={data}>
         <defs>
@@ -47,7 +37,7 @@ const TemperaturaHora = ({tempHora,horaT}) => {
           </linearGradient>
         </defs>
         <Area type="monotone" dataKey="Temp" stroke="#c4c72e" fillOpacity={1} fill="url(#tempHora)" />
-        <XAxis dataKey="name" tick={{fontSize: 15}}  />
+        <XAxis dataKey="name" tick={{fontSize: 15}} interval={1} />
         <YAxis width={40} tick={{fontSize: 15}}/>
         <Tooltip />
       </AreaChart>
@@ -57,7 +47,7 @@ const TemperaturaHora = ({tempHora,horaT}) => {
   return (
     <Hora>
       <TempHora>Temperatura por Hora</TempHora>
-      {renderLineChart}
+      {graficoTemp}
     </Hora>
   )
 }
